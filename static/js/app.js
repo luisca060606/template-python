f8ab4739cd70c41dedf76973ceafcb7b9ad98888
luis <luis.ca0804@gmail.com>
2023-10-13 15:13:57 -0400

    add files

---
+++ static/js/app.js
@@ -0,0 +1,18 @@
+
+var app = angular.module('bonusApp',[ ]);
+
+app.controller('mainCtrl', ['$scope', function($scope){
+
+	$scope.formData = {};
+
+	$scope.guardar_datos = function(valido) {
+		if (!valido) {
+			return;
+		}
+		console.log("posteando...");
+	}
+
+
+
+	
+}]);
